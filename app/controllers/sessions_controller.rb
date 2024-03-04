d2ab5bd4902f7c1e78d21d1813a172429246cb2b
Ankush  Halbe <ankush.halbe@kellogg.northwestern.edu>
2024-03-04 16:13:09 +0000

    woof

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if @user["password"] == params["password"]
+    flash["notice"] = "Welcome" 
+    redirect_to "/places"
+      else
+      flash["notice"] = "Nope" 
+      redirect_to "/login"
+      end
+    else
+    flash["notice"] = "Nope" 
+    redirect_to "/login"    
+    end
+end 
 
   def destroy
+    #flash ["notice"] = "Goodbye" 
+    #redirect_to "/login"
   end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+
   resources "entries"
   resources "places"
   resources "sessions"
   resources "users"
+
+  get "/login", :controller => "users", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
 end
