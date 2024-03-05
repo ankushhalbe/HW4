0edb85f1c28a26e423147831d953f6920d0d32d5
Ankush  Halbe <ankush.halbe@kellogg.northwestern.edu>
2024-03-05 06:49:28 +0000

    cc

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by({"email" => params["email"]})
     if @user != nil
-      if @user["password"] == params["password"]
-    flash["notice"] = "Welcome" 
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome" 
     redirect_to "/places"
       else
       flash["notice"] = "Nope" 
@@ -19,6 +20,7 @@ class SessionsController < ApplicationController
 end 
 
   def destroy
+    session["user_id"] = nil
     flash["notice"] = "Goodbye" 
     redirect_to "/login"
   end
